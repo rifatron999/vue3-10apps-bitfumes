72c8b5603f7bb81bb3f24468faa5730148336f41
rifatron999 <rifatron999@gmail.com>
2022-11-25 23:01:20 +0600

    first vue app src/main.js 02

--- First-vue-app/src/main.js
+++ First-vue-app/src/main.js
@@ -1,11 +1,11 @@
-const app = Vue.createApp({
-    data(){
-        return {
-            title: 'Vue3 Tutorial',
-        };
-    },
-    template: `<div>
-        <h1> Hii {{ title }}</h1>
-        </div>`
-});
-app.mount("#vue-app");
+const template = `<div>
+    <h1> Hii {{ title }}</h1>
+    </div>`;
+
+const data = function data(){
+    return {
+        title: 'Vue3 Tutorial',
+    };
+}
+const APP = { data,template };
+const app = Vue.createApp(APP).mount("#vue-app");
