2eaafaa29130470b001aad5fe1446a4e6f330d4a
rifatron999 <rifatron999@gmail.com>
2022-12-03 13:33:12 +0600

    Template Syntax Text Interpolation

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,4 +1,14 @@
 <template>
-  <h1>Hey vue3 from cli</h1>
+  <h1>{{title.toUpperCase()}}</h1>
   <p>Someting else</p>
 </template>
+
+<script>
+  export default{
+    data() {
+      return {
+        title:"hey vue3",
+      }
+    }
+  }
+</script>
