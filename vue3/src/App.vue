b32537aca5a1e066f6834a13391b83f7c17a5703
rifatron999 <rifatron999@gmail.com>
2022-12-03 14:50:20 +0600

    Template Syntax directive v-if v-else-if v-else

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1 v-html="title"></h1>
+  <h1 v-if="(2*3 == 5)" v-html="title"></h1>
+  <h3 v-else-if="(2*2 === 4)">this is else if</h3>
+  <h2 v-else >This is v-else part</h2>
+
   <p>Someting else</p>
 </template>
 
@@ -8,6 +11,7 @@
     data() {
       return {
         title:"<h1> hey vue3 from h1</h1>",
+        isShowing: true,
       }
     }
   }
